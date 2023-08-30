4ca1102af4b74345936425de5a185743a50d74d7
jaymhorsh <jaji.moshood97@yahoo.com>
2023-08-30 17:43:34 +0100

    add scroll reveal

--- public/js/scrollreveal.js
+++ public/js/scrollreveal.js
@@ -1,14 +1,14 @@
-// const sr = ScrollReveal({
-//     origin: "top",
-//     distance: "30px",
-//     duration: 1500,
-//     reset: true,
-// });
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duration: 1500,
+    reset: true,
+});
 
-// sr.reveal(
-//     `.home-img,.home-text,
-//     .facilities-content,
-//     .arrow,.item,.service-img`, {
-//         interval: 150,
-//     }
-// );
+sr.reveal(
+    `.home-img,.home-text,
+    .facilities-content,
+    .arrow,.item,.service-img`, {
+        interval: 150,
+    }
+);
